701904481154d3b49091bcf93120461b8a98f8f4
Albert Stanica <albertstanica@gmail.com>
2021-11-11 22:18:52 -0400

    First Commit

---
+++ Assets/scripts/movePlatform.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlatform : MonoBehaviour
+{
+    public GameObject pathStart;
+    public GameObject pathEnd;
+    public int speed;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    void Start()
+    {
+        startPosition = pathStart.transform.position;
+        endPosition = pathEnd.transform.position;
+        StartCoroutine(Vector3LerpCoroutine(gameObject, endPosition, speed));
+    }
+
+    void Update()
+    {
+        if (transform.position == endPosition)
+        {
+            StartCoroutine(Vector3LerpCoroutine(gameObject, startPosition, speed));
+        }
+        if (transform.position == startPosition)
+        {
+            StartCoroutine(Vector3LerpCoroutine(gameObject, endPosition, speed));
+        }
+    }
+
+    IEnumerator Vector3LerpCoroutine(GameObject obj, Vector3 target, float speed)
+    {
+        Vector3 startPosition = obj.transform.position;
+        float time = 0f;
+
+        while (obj.transform.position != target)
+        {
+            obj.transform.position = Vector3.Lerp(startPosition, target, (time / Vector3.Distance(startPosition, target)) * speed);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        col.gameObject.transform.SetParent(gameObject.transform, true);
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        col.gameObject.transform.parent = null;
+    }
+}
